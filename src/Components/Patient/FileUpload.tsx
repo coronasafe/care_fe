a8d9b3657f8aebfa86a8f1daf6d2d1e82e3a9915
Zishan Ahmad <zishan.barun@gmail.com>
2024-03-13 12:25:53 +0530

    fixed unnecessary placement of mic permission info

--- src/Components/Patient/FileUpload.tsx
+++ src/Components/Patient/FileUpload.tsx
@@ -226,6 +226,7 @@ export const FileUpload = (props: FileUploadProps) => {
     { name: "Unarchived Files", value: "UNARCHIVED" },
     { name: "Archived Files", value: "ARCHIVED" },
   ]);
+  const [isMicPermission, setIsMicPermission] = useState(true);
 
   const { data: patient } = useQuery(routes.getPatient, {
     pathParams: { id: patientId },
@@ -554,6 +555,10 @@ export const FileUpload = (props: FileUploadProps) => {
     }
 
     setbtnloader(false);
+  };
+
+  const handleSetMicPermission = (isPermitted: boolean) => {
+    setIsMicPermission(isPermitted);
   };
 
   const renderFileUpload = (item: FileUploadModel) => {
@@ -1490,8 +1495,9 @@ export const FileUpload = (props: FileUploadProps) => {
                       confirmAudioBlobExists={confirmAudioBlobExists}
                       reset={resetRecording}
                       setResetRecording={setResetRecording}
+                      handleSetMicPermission={handleSetMicPermission}
                     />
-                    {!audioBlobExists && (
+                    {!audioBlobExists && !isMicPermission && (
                       <span className="text-sm font-medium text-warning-500">
                         <CareIcon
                           icon="l-exclamation-triangle"
